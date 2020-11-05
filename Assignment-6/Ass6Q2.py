02a4fcbea7a6a3c952b185b9f089a881b48b5ad0
apbadhani <42250690+apbadhani@users.noreply.github.com>
2020-11-05 14:11:14 +0530

    Update Ass6Q2.py

--- Assignment-6/Ass6Q2.py
+++ Assignment-6/Ass6Q2.py
@@ -3,17 +3,18 @@ f=li.fu2
 c=0
 d=1
 '''''
-Max error = 0.0005 for good approx
+Max error = 0.001 for good approx
 N calculated:
-for midpoint = 50
-for trapezoid = 70
-for simpson = 100
+for midpoint = 10
+for trapezoid = 13
+for simpson = 4
 '''''
-print("I value for Midpoint = ",li.midpoint(c,d,50,f))
-print("I value for Trapezoid = ",li.trapezoid(c,d,70,f))
-print("I value for Simpson = ",li.simpson(c,d,100,f))
+print("I value for Midpoint = ",li.midpoint(c,d,10,f))
+print("I value for Trapezoid = ",li.trapezoid(c,d,13,f))
+print("I value for Simpson = ",li.simpson(c,d,4,f))
 '''''
-I value for Midpoint =  0.7468363957465884
-I value for Trapezoid =  0.7370225141840184
-I value for Simpson =  0.7524525977629901
+OUTPUT:
+I value for Midpoint =  0.7471308777479975
+I value for Trapezoid =  0.7464612610366896
+I value for Simpson =  0.7468553797909872
 '''''
